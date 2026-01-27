1ae6b3e8995b48373ac0b49c4058d59d28eb5911
Amey Deshmukh <ameydeshmukh.work@gmail.com>
2026-01-27 23:24:46 +0530

    Temperature Converter

--- Level 1/TemperatureConverter.java
+++ Level 1/TemperatureConverter.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class TemperatureConverter {
+
 public static double celsiusToFahrenheit(double celsius) {
         return (celsius * 9/5) + 32;
 
@@ -27,51 +28,74 @@ public static double celsiusToFahrenheit(double celsius) {
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int choice;
+        double input,result;
+
          System.out.println("Temperature Converter");
 
-        // Convert Celsius to Fahrenheit
-
-        System.out.println("Enter temperature in Celsius:");
-        double celsius = scanner.nextDouble();    
-        double fahrenheit = celsiusToFahrenheit(celsius);
-        System.out.printf("%.2f Celsius is %.2f Fahrenheit%n", celsius, fahrenheit);  
-
-        // Convert Fahrenheit to Celsius
-
-        System.out.println("Enter temperature in Fahrenheit:");        
-        double fahrenheitInput = scanner.nextDouble();    
-        double celsiusConverted = fahrenheitToCelsius(fahrenheitInput);
-        System.out.printf("%.2f Fahrenheit is %.2f Celsius%n", fahrenheitInput, celsiusConverted);
-          
-
-        //Celsius to Kelvin
-        System.out.println("Enter temperature in Celsius:");
-        double celsiusInput = scanner.nextDouble();
-        double kelvin = celsiusToKelvin(celsiusInput);
-        System.out.printf("%.2f Celsius is %.2f Kelvin%n", celsiusInput, kelvin);
-
-       //  Kelvin to Celsius
-        System.out.println("Enter temperature in Kelvin:");
-        double kelvinInput = scanner.nextDouble();  
-        double celsiusFromKelvin = kelvinToCelsius(kelvinInput);
-        System.out.printf("%.2f Kelvin is %.2f Celsius%n", kelvinInput, celsiusFromKelvin); 
+         do {
+            System.out.println("Choose conversion:");
+            System.out.println("1. Celsius to Fahrenheit");
+            System.out.println("2. Fahrenheit to Celsius");
+            System.out.println("3. Celsius to Kelvin");
+            System.out.println("4. Kelvin to Celsius");
+            System.out.println("5. Fahrenheit to Kelvin");
+            System.out.println("6. Kelvin to Fahrenheit");
+            System.out.println("7. Exit");
+              System.out.println("Enter your preferred option: ");
+            choice = scanner.nextInt();
     
-
-        // Fahrenheit to Kelvin
-
-        System.out.println("Enter temperature in Fahrenheit:");
-        double fahrenheitInput2 = scanner.nextDouble();
-        double kelvinFromFahrenheit = fahrenheitToKelvin(fahrenheitInput2);
-        System.out.printf("%.2f Fahrenheit is %.2f Kelvin%n", fahrenheitInput2, kelvinFromFahrenheit);
         
-    // Kelvin to Fahrenheit
-        System.out.println("Enter temperature in Kelvin:");
-        double kelvinInput2 = scanner.nextDouble();
-        double fahrenheitFromKelvin = kelvinToFahrenheit(kelvinInput2);
-        System.out.printf("%.2f Kelvin is %.2f Fahrenheit%n", kelvinInput2, fahrenheitFromKelvin);
-    scanner.close(); 
+
+         switch (choice) {
+
+            case 1:
+                System.out.println("Enter temperature in Celsius:");
+                input = scanner.nextDouble();
+                result = celsiusToFahrenheit(input);
+                System.out.printf("%.2f Celsius is %.2f Fahrenheit%n", input, result);
+                break;
+            case 2:
+                System.out.println("Enter temperature in Fahrenheit:");
+                input = scanner.nextDouble();
+                result = fahrenheitToCelsius(input);
+                System.out.printf("%.2f Fahrenheit is %.2f Celsius%n", input, result);
+                break;
+            case 3:
+                System.out.println("Enter temperature in Celsius:");
+                input = scanner.nextDouble();
+                result = celsiusToKelvin(input);
+                System.out.printf("%.2f Celsius is %.2f Kelvin%n", input, result);
+                break;
+            case 4:
+                System.out.println("Enter temperature in Kelvin:");
+                input = scanner.nextDouble();
+                result = kelvinToCelsius(input);
+                System.out.printf("%.2f Kelvin is %.2f Celsius%n", input, result);
+                break;
+            case 5:
+                System.out.println("Enter temperature in Fahrenheit:");
+                input = scanner.nextDouble();
+                result = fahrenheitToKelvin(input);
+                System.out.printf("%.2f Fahrenheit is %.2f Kelvin%n", input, result);
+                break;
+            case 6:
+                System.out.println("Enter temperature in Kelvin:");
+                input = scanner.nextDouble();
+                result = kelvinToFahrenheit(input);
+                System.out.printf("%.2f Kelvin is %.2f Fahrenheit%n", input, result);
+                break;
+            case 7:
+                System.out.println("Exiting...");
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+              
+         }
+        }
+      while (choice != 7);
+    scanner.close();    
+
    
     }
 }
-
-    
