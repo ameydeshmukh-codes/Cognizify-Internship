64067ccfa7912eeea58eec922df845a316c5efae
Amey Deshmukh <ameydeshmukh.work@gmail.com>
2026-01-27 23:56:54 +0530

    Palindrome Checker

---
+++ Level 1/PalindromeChecker.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class PalindromeChecker {
+ 
+    public static boolean isPalindrome(String str) {
+       String cleaned = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0;
+        int right = cleaned.length() - 1;
+
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word or phrase:");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}   
+
+
+
